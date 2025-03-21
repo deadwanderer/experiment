fa9939aa437a777c8c14395a72802610538315af
deadwanderer <anthonyjessica822@gmail.com>
2025-03-20 22:47:14 -0400

    Shader tweak to show three textures

--- assets/shaders/TexturedQuadArray.frag.hlsl
+++ assets/shaders/TexturedQuadArray.frag.hlsl
@@ -2,6 +2,6 @@ Texture2DArray<float4> Texture : register(t0, space2);
 SamplerState Sampler : register(s0, space2);
 
 float4 main(float2 TexCoord : TEXCOORD0) : SV_Target0 {
-    uint array_index = uint(int(TexCoord.y > 0.5f));
+    uint array_index = TexCoord.x > 0.67f ? 2 : TexCoord.x > 0.33f ? 1 : 0;
     return Texture.Sample(Sampler, float3(TexCoord, float(array_index)));
 }
